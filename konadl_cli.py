a7fd9e9d822e1ed6d353ee8d6bcba189f208c26f
K4YT3X <k4t@protonmail.com>
2018-04-15 01:53:17 -0400

    v1.2: save & continue downloading progress

--- konadl_cli.py
+++ konadl_cli.py
@@ -16,7 +16,7 @@ import argparse
 import os
 import traceback
 
-VERSION = '1.1'
+VERSION = '1.2'
 
 
 def process_arguments():
@@ -32,6 +32,7 @@ def process_arguments():
     control_group.add_argument('-p', '--page', help='Crawl a specific page', type=int, action='store', default=False)
     control_group.add_argument('-y', '--yandere', help='Crawl Yande.re site', action='store_true', default=False)
     control_group.add_argument('-o', '--storage', help='Storage directory', action='store', default=False)
+    control_group.add_argument('--progress', help='Specify progress file to load', action='store', default='konadl.progress')
     threading_group = parser.add_argument_group('Ratings')
     control_group.add_argument('-s', '--safe', help='Include Safe rated images', action='store_true', default=False)
     control_group.add_argument('-q', '--questionable', help='Include Questionable rated images', action='store_true', default=False)
@@ -51,7 +52,7 @@ def check_storage_dir(args):
     will recognize.
     """
     if args.storage is False:
-        return '/tmp/konachan/'
+        return False
     storage = args.storage
     if storage[-1] != '/':
         storage += '/'
@@ -79,7 +80,7 @@ def check_storage_dir(args):
     return storage
 
 
-def display_options(kona):
+def display_options(kona, load_progress):
     """ Display konadl crawling options
 
     Prints the options of konadl before start crawling.
@@ -91,33 +92,33 @@ def display_options(kona):
     """
     avalon.dbgInfo('Program Started')
     avalon.info('Using storage directory: {}{}'.format(avalon.FG.W, kona.storage))
-    if kona.safe:
-        avalon.info('Including {}{}SAFE{}{} rated images'.format(avalon.FG.W, avalon.FM.BD, avalon.FM.RST, avalon.FG.G))
-    if kona.questionable:
-        avalon.warning('Including {}QUESTIONABLE{} rated images'.format(avalon.FG.W, avalon.FG.Y))
-    if kona.explicit:
-        avalon.warning('Including {}EXPLICIT{} rated images'.format(avalon.FG.R, avalon.FG.Y))
-    if kona.yandere:
-        avalon.info('Crawling yande.re')
+    if load_progress:
+        avalon.info('Sourcing configuration defined in the progress file')
+    else:
+        if kona.safe:
+            avalon.info('Including {}{}SAFE{}{} rated images'.format(avalon.FG.W, avalon.FM.BD, avalon.FM.RST, avalon.FG.G))
+        if kona.questionable:
+            avalon.warning('Including {}QUESTIONABLE{} rated images'.format(avalon.FG.W, avalon.FG.Y))
+        if kona.explicit:
+            avalon.warning('Including {}EXPLICIT{} rated images'.format(avalon.FG.R, avalon.FG.Y))
+        if kona.yandere:
+            avalon.info('Crawling yande.re')
 
-    if args.pages:
-        if args.pages == 1:
-            avalon.info('Crawling {}{}{}{}{} Page\n'.format(avalon.FG.W, avalon.FM.BD, args.pages, avalon.FM.RST, avalon.FG.G))
-        else:
-            avalon.info('Crawling {}{}{}{}{} Pages\n'.format(avalon.FG.W, avalon.FM.BD, args.pages, avalon.FM.RST, avalon.FG.G))
-    elif args.all:
-        avalon.warning('Crawling {}{}ALL{}{} Pages\n'.format(avalon.FG.W, avalon.FM.BD, avalon.FM.RST, avalon.FG.G))
-    elif args.page:
-        avalon.info('Crawling Page #{}'.format(args.page))
+        if args.pages:
+            if args.pages == 1:
+                avalon.info('Crawling {}{}{}{}{} Page\n'.format(avalon.FG.W, avalon.FM.BD, args.pages, avalon.FM.RST, avalon.FG.G))
+            else:
+                avalon.info('Crawling {}{}{}{}{} Pages\n'.format(avalon.FG.W, avalon.FM.BD, args.pages, avalon.FM.RST, avalon.FG.G))
+        elif args.all:
+            avalon.warning('Crawling {}{}ALL{}{} Pages\n'.format(avalon.FG.W, avalon.FM.BD, avalon.FM.RST, avalon.FG.G))
+        elif args.page:
+            avalon.info('Crawling Page #{}'.format(args.page))
 
     avalon.info('Opening {}{}{}{}{} crawler threads'.format(avalon.FG.W, avalon.FM.BD, args.crawlers, avalon.FM.RST, avalon.FG.G))
     avalon.info('Opening {}{}{}{}{} downloader threads\n'.format(avalon.FG.W, avalon.FM.BD, args.downloaders, avalon.FM.RST, avalon.FG.G))
 
 
-args = process_arguments()
-
-
-class konadl_linux(konadl):
+class konadl_avalon(konadl):
     """ Overwrite original methods for better
     appearance and readability.
     """
@@ -126,8 +127,11 @@ class konadl_linux(konadl):
         avalon.warning('[Main Thread] KeyboardInterrupt Caught!')
         avalon.warning('[Main Thread] Flushing queues and exiting')
 
-    def print_retrieval(self, url):
-        avalon.dbgInfo("Retrieving: {}".format(url))
+    def print_saving_progress(self):
+        avalon.info('[Main Thread] Saving progress')
+
+    def print_retrieval(self, page, url):
+        avalon.dbgInfo("[Page={}] Retrieving: {}".format(page, url))
 
     def print_crawling_page(self, page):
         avalon.info('Crawling page: {}{}#{}'.format(avalon.FM.BD, avalon.FG.W, page))
@@ -135,10 +139,16 @@ class konadl_linux(konadl):
     def print_thread_exit(self, name):
         avalon.dbgInfo('[libkonadl] {} thread exiting'.format(name))
 
+    def print_faulty_progress_file(self):
+        avalon.error('Faulty progress file!')
+        avalon.error('Aborting\n')
+
+
+args = process_arguments()
 
 try:
     if __name__ == '__main__':
-        kona = konadl_linux()  # Create crawler object
+        kona = konadl_avalon()  # Create crawler object
         kona.icon()
 
         if args.version:  # prints program legal / dev / version info
@@ -150,16 +160,31 @@ try:
             print('Contact: k4yt3x@protonmail.com\n')
             exit(0)
 
+        # If progress file exists
+        # Ask user if he or she wants to load it
+        load_progress = False
+        if os.path.isfile(args.progress):
+            avalon.info('Progress file found')
+            if avalon.ask('Continue from where you left off?', True):
+                kona.load_progress = True
+                kona.progress_file = args.progress
+                load_progress = True
+            else:
+                avalon.info('Creating new download profile')
+
         kona.storage = check_storage_dir(args)
+        if kona.storage is False:
+            avalon.error('Please specify storage directory\n')
+            exit(1)
         kona.yandere = args.yandere
         kona.safe = args.safe
         kona.questionable = args.questionable
         kona.explicit = args.explicit
         kona.post_crawler_threads_amount = args.crawlers
         kona.downloader_threads_amount = args.downloaders
-        display_options(kona)
+        display_options(kona, load_progress)
 
-        if not kona.safe and not kona.questionable and not kona.explicit:
+        if not kona.safe and not kona.questionable and not kona.explicit and not load_progress:
             avalon.error('Please supply information about what you want to download')
             print(avalon.FM.BD + 'You must include one of the following arguments:')
             print('  -s, --safe            Include Safe rated images')
@@ -167,7 +192,7 @@ try:
             print('  -e, --explicit        Include Explicit rated images')
             print('Use --help for more information\n' + avalon.FM.RST)
             exit(1)
-        elif not args.pages and not args.all and not args.page:
+        elif not args.pages and not args.all and not args.page and not load_progress:
             avalon.error('Please supply information about what you want to download')
             print(avalon.FM.BD + 'You must include one of the following arguments:')
             print('  -n PAGES, --pages PAGES')
@@ -176,14 +201,17 @@ try:
             print('  -p PAGE, --page PAGE  Crawl a specific page')
             print('Use --help for more information\n' + avalon.FM.RST)
 
-        if args.pages:
-            kona.crawl(args.pages)
+        if load_progress:
+            kona.crawl()
+        elif args.pages:
+            kona.pages = args.pages
+            kona.crawl()
         elif args.all:
-            kona.crawl_all()
+            kona.crawl_all_pages()
         elif args.page:
             kona.crawl_page(args.page)
 
-    avalon.info('Konachan Downloader main thread exited without errors\n')
+    avalon.info('Main thread exited without errors\n')
 
 except KeyboardInterrupt:
     avalon.warning('Ctrl+C detected in CLI Module')
